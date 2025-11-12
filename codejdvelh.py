b80ba92568cff5ce29bc059fb3381084c7392714
thomascloarec2007-lang <thomas.cloarec2007@gmail.com>
2025-11-12 16:08:25 +0100

    Update codejdvelh.py

--- codejdvelh.py
+++ codejdvelh.py
@@ -3,6 +3,8 @@ import os
 # Déclaration des variables représentant des caractéristiques du joueur pour une utilisation future
 pv = 20
 pm = 0
+from random import randit 
+
 # Introduction
 print("Vous vous réveillez amnésique et affaibli en dessous d'un grand arbre en ammont d'une colline arride ")
 print("Vous regardez vos effets personnels")
@@ -60,20 +62,84 @@ if int(reponse_1) == 2:
         print("Vous rentrer dans une forêt")
         print("Vous recroisez l'étrangé à un tournant, qui vous regarde d'un air menaçant...")
         print("Il séagissait d'un piège... Il ne faut jamais faire confiance aux personnes moches...")
+        
+
+
+    # Branche 2-2
+    if int(reponse_1_1) == 2:
+        print("Vous partez dans la direction opposée")
+        print("Vous recroisez l'étrangé à un tournant, qui vous regarde d'un air menaçant...")
+        print("Il séagissait d'un piège... Les moches sont toujours vicieux...")
         print("L'homme s'élance ver vous, armé d'une dague !!!")
-        print("Vous décidez de : ")
+        print("Vous décidez de: ")
         print("1)Se diriger vers la voie \n 2)")
         reponse_1_1_2 = input("Que choisissez-vous ? (sélectionnez le numéro) : ")
 
-        # Branche 2-1-1
-        if int(reponse_1_1_2) == 1:
-            print("Vous l'accostez et la lui rendez")
-            print("Pour vous remercier, l'inconnu vous invite à boire un coup")
-            print("To be continued...")
+    print("L'homme s'élance ver vous, armé d'une dague !!!")
+    print("Vous devez vous battre !!!")
+    pv_du_moche = 10
+    stam = 100
+    while pv > 0 or pv_du_moche > 0:
+        #defense:
+        if stam <= 0:
+            print ("vous n'avez pas la force d'esquiver...")
+            pv -= 5
+        else:
+            
+            attaque = randit(1,2)
+            print("vous esquivez: 1)vers la droite 2)vers la gauche")
+            reponse_1_1 = input("Que choisissez-vous? (sélectionnez le numéro) : ")
+            if reponse_1_1 == attaque:
+                chance = randit(1, 100)
+                if chance > 90:
+                    pv -= 2 
+                    print(" Bonne esquive !!! Mais vous êtes tout de même touché !!!")
+                else:
+                    print ("Bien joué !!")
+            if reponse_1_1 != attaque:
+                pv -= 5
+                print(" Mauvais coté...Vous subissez d'imortant dommage... ")
+    
+        #attaque
+        if stam <= 0:
+            print("vous êtes à cours d'endurance..")
+            stam += 20 
+        else:
+            print("A votre tour, choisissez votre attque")
+            print("1)attaque vive, 2)attaque basique, 3)attaque lourde, 4)se reposer ")
+            attaque = intput("choisissez une action")
+            if attaque == 1:
+                stam -= 10
+                chance = randit (1,100)
+                if chance > 90:
+                    print ("votre attaque échoue...")
+                else:
+                    print("Bravo vous infligez des dégâts!!! ")
+                    pv_du_moche -= 2
+                
+            if attaque == 2:
+                stam -= 20
+                chance = randit (1,100)
+                if chance > 80:
+                    print ("votre attaque échoue...") 
+                else:
+                    print("Bravo vous infligez des dégâts!!! ")
+                    pv_du_moche -= 5
+            if attaque == 3:
+                stam -= 30
+                chance = randit (1,100)
+                if chance > 25:
+                    print ("votre attaque échoue...") 
+                else:
+                    print("Bravo vous infligez des dégâts!!! ")
+                    pv_du_moche -= 8
+            else:
+                stam += 20
+                print("Vous vous sentez remplit d'énergie !")
 
-        # Branche 2-1-2
-        elif int(reponse_1_1_2) == 2:
-            print("Vous mettez la clef discretement dans votre poche")
-            print("Vous continuez votre route")
-            print("To be continued...")
+            
+            
+            
+        
+    
 
